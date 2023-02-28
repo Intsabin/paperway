e03ab776156f20d1b2278527ff4a1be129b4fe69
sudarshan <sudarshandn1999@gmail.com>
2023-02-28 14:22:16 +0545

    intialiaze ecomm

---
+++ contact.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title> contact us</title>
+    </head>
+
+    <body>
+        <?php include 'includes/session.php' ?>
+        <?php include 'includes/header.php' ?>
+        <?php include 'includes/navbar.php' ?>
+    <div class="row">
+	<div class='box box-solid'>
+	  	<div class='box-header with-border'>
+	    	<h3 class='box-title'><b>Follow us on Social Media</b></h3>
+	  	</div>
+	  	<div class='box-body'>
+	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+	    	<a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
+	    	<a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+	    	<a class="btn btn-social-icon btn-google"><i class="fa fa-google-plus"></i></a>
+	    	<a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
+	  	</div>
+	</div>
+</div>
+<?php include 'includes/footer.php' ?>
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
